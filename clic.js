184e24dfb7f67f45ceae12d367be5f0969e1a38a
Humphrey.... <153418064+hum-phrey7@users.noreply.github.com>
2024-12-19 20:30:13 +0000

    Add files via upload

---
+++ clic.js
@@ -0,0 +1,11 @@
+
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.querySelector('.but').addEventListener('click', function () 
+  {
+    header.style.color = 'blue';
+  });
+  
+}) ();
